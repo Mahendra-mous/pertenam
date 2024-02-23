1bf25f716c296564e3a8d3abbc34c63fd067d66c
Mahendra-mous <hutamahendra830@gmail.com>
2024-02-23 13:51:00 +0700

    Initial commit

---
+++ app/src/main/java/com/example/pertenam/Topic.kt
@@ -0,0 +1,10 @@
+package com.example.pertenam.model
+
+import androidx.annotation.DrawableRes
+import androidx.annotation.StringRes
+
+data class Topic(
+    @StringRes val name: Int,
+    val availableCourses: Int,
+    @DrawableRes val imageRes: Int
+)
